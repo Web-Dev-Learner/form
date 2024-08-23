7af632d4197066224326b6fa67b006bdd1f4e632
Web-Dev-Learner <shribabe2@gmail.com>
2024-08-23 16:41:04 +0530

    updated styles

--- formcraft/src/components/Home.jsx
+++ formcraft/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-gray-800">
-        Welcome to Form Craft
+        Welcome to FormCraft
       </h1>
     </div>
   );
